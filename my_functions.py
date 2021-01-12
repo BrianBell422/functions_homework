25fa6d1ab45c69240bf648eef78bd646e7b13db8
BrianBell422 <brianbell.uk@gmail.com>
2021-01-12 19:23:27 +0000

    Homework completed update

--- my_functions.py
+++ my_functions.py
@@ -1,66 +1,66 @@
 # Returning a value from a function
 
-# def greet():
-#     return "Hey"
+def greet():
+    return "Hey"
 
-# greeting = greet()
-# print(greeting)
+greeting = greet()
+print(greeting)
 
 
 # Paramaters and arguments
 
-# def greet(name): 
-#   return "Hey " + name
+def greet(name): 
+  return "Hey " + name
 
-# greeting = greet("Brian")
-# print(greeting)
+greeting = greet("Brian")
+print(greeting)
 
 
 # Multiple parameters
 
-# def greet(name, time_of_day):
-#     return "Good " + time_of_day + ", " + name
+def greet(name, time_of_day):
+    return "Good " + time_of_day + ", " + name
 
-# greeting = greet('Brian', 'morning')
-# print(greeting)
+greeting = greet('Brian', 'morning')
+print(greeting)
 
 
 # Laundry
 
-# def wash_clothes(laundry, detergent, water):
-#     clean_laundry = laundry + detergent + water
-#     return clean_laundry
+def wash_clothes(laundry, detergent, water):
+    clean_laundry = laundry + detergent + water
+    return clean_laundry
 
-# clean_tshirt = wash_clothes('dirty tshirt, ', 'liquid detergent, ', 'water')
-# print(clean_tshirt)
+clean_tshirt = wash_clothes('dirty tshirt, ', 'liquid detergent, ', 'water')
+print(clean_tshirt)
 
 
 # Passing variables as arguments
 
-# def greet(name, time_of_day):
-#   return "Good " + time_of_day + ", " + name
+def greet(name, time_of_day):
+  return "Good " + time_of_day + ", " + name
 
-# name_1 = "Brian"
-# time_of_day_1 = "evening"
-# greeting = greet(name_1, time_of_day_1)
-# print(greeting)
+name_1 = "Brian"
+time_of_day_1 = "evening"
+greeting = greet(name_1, time_of_day_1)
+print(greeting)
 
-# name_2 = "Ada"
-# time_of_day_2 = "afternoon"
-# greeting = greet(name_2, time_of_day_2)
-# print(greeting)
+name_2 = "Ada"
+time_of_day_2 = "afternoon"
+greeting = greet(name_2, time_of_day_2)
+print(greeting)
 
 # Scope
 
-# def greet():
-#   words = "Hey"
-#   return words
+def greet():
+  words = "Hey"
+  return words
 
-# def greet_two():
-#   words = "Hi"
-#   return words
+def greet_two():
+  words = "Hi"
+  return words
 
-# print(greet_two())
+print(greet_two())
 
 
 # More complex example
